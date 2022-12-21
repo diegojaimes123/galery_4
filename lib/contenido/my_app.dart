0c790073c0496ab37589c6834c86569bd7a50cb0
diegojaimes123 <110691669+diegojaimes123@users.noreply.github.com>
2022-12-21 04:56:16 -0500

    archivos

---
+++ lib/contenido/my_app.dart
@@ -0,0 +1,57 @@
+import 'package:flutter/material.dart';
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'flutter layout demo ',
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('flutter layout demo'),
+        ),
+        body: Center(child: _buildImageColum()),
+
+      ),
+    );
+  }
+
+
+  Widget _buildImageColum() {
+    return Container(
+      decoration: const BoxDecoration(
+        color: Colors.black26,
+      ),
+      child: SingleChildScrollView(
+        child: Column(
+          children: [
+            _buildImageRow(1),
+            _buildImageRow(3),
+          ],
+        ),
+      ),
+    );
+  }
+  Widget _buildDecoratedImage(int imageIndex) => Expanded(
+    child: Container(
+      decoration: BoxDecoration(
+        border: Border.all(width: 10,color: Colors.black38),
+        borderRadius: const BorderRadius.all(Radius.circular(8)),
+      ),
+      margin: const EdgeInsets.all(4),
+      child: Image.asset('images/pic$imageIndex.jpg'),
+    ),
+  );
+
+
+  Widget _buildImageRow(int imageIndex) => Row(
+    children: [
+      _buildDecoratedImage(imageIndex),
+      _buildDecoratedImage(imageIndex + 1),
+    ],
+
+  );
+
+
+}
